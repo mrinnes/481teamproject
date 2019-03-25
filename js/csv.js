9ca12745a6298aa2f316618684c51602b09c5d16
noahsung1 <noahcrimmins@yahoo.com>
2019-03-25 15:11:58 -0400

    enabled printing of array

--- js/csv.js
+++ js/csv.js
@@ -1,19 +1,25 @@
 /* Function that takes school and scores and prints CSV file */
 function buildCSV(school,score) {
-  var combined = school + "," + score;
   var fs = require('fs');
-  var stream = fs.createWriteStream("my_file.csv");
-  stream.once('open', function(fd) {
-    stream.write(school + "," + score);
-    stream.end();
+  fs.open('scores.csv', 'w', function (err, file) {
+    if (err) throw err;
   });
+  var i;
+  for (i = 0; i < school.length; i++) {
+    var combined = school[i] + "," + score[i];
+    fs.appendFile('scores.csv', combined + '\n', function (err) {
+      if (err) throw err;
+    });
+  }
+
 
 }
 
 /* Function that takes school and scores and prints CSV file */
-function downloadCSV(school,score) {
-  console.log("test 2");
-
+function downloadCSV() {
+  var schoolArr = ["EMU", "MSU", "U of M"];
+  var scoreArr  = [10,20,30];
+  buildCSV(schoolArr,scoreArr);
 }
 
-buildCSV("EMU",10);
+downloadCSV();
