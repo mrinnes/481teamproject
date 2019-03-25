e35b2ac4b806f7885ecc31c2b848d888c6363c8b
noahsung1 <noahcrimmins@yahoo.com>
2019-03-25 17:49:15 -0400

    function now prints information from database

--- js/csv.js
+++ js/csv.js
@@ -1,25 +1,45 @@
-/* Function that takes school and scores and prints CSV file */
+var express         = require("express");
+var mongoose        = require("mongoose");
+var Questions       = require("../models/MultiChoice.js");
+
+mongoose.connect("mongodb://localhost/Icompute",{ useNewUrlParser: true });
+
+/* Function that creates a .csv file and adds school name and scores */
 function buildCSV(school,score) {
-  var fs = require('fs');
-  fs.open('scores.csv', 'w', function (err, file) {
+  var fs = require('fs'); //Create File Stream
+  fs.open('scores.csv', 'w', function (err, file) { //Create the .csv file, throws error if error
     if (err) throw err;
   });
-  var i;
-  for (i = 0; i < school.length; i++) {
-    var combined = school[i] + "," + score[i];
-    fs.appendFile('scores.csv', combined + '\n', function (err) {
+  var i; //Creates counting variable for the for loop.
+  for (i = 0; i < school.length; i++) { //Run loop once for each school in school array
+    var combined = school[i] + "," + score[i]; //Combines the school and scores, and seperates by comma.
+    fs.appendFile('scores.csv', combined + '\n', function (err) { //Writes (appends) to the file.
       if (err) throw err;
     });
   }
-
-
 }
 
-/* Function that takes school and scores and prints CSV file */
+/* Function that takes school and scores and passes it to buildCSV */
 function downloadCSV() {
-  var schoolArr = ["EMU", "MSU", "U of M"];
-  var scoreArr  = [10,20,30];
-  buildCSV(schoolArr,scoreArr);
+  var schoolArr = [];
+  var scoreArr = [];
+  var u = Questions.find({}, { question: 1, option_A: 1, _id: 0}, function(err, data){
+    if(err){
+      console.log(err);
+    }else{
+      //Set school names
+      data.forEach(function(row){
+        schoolArr.push(row.question);
+      })
+
+      //Set score
+      data.forEach(function(row){
+        scoreArr.push(row.option_A);
+      })
+
+      buildCSV(schoolArr,scoreArr);
+    }
+  })
 }
 
 downloadCSV();
