db1fd184f6178a4ddf365c46531d1d0d905feb17
dbrowder <47614101+dbrowder@users.noreply.github.com>
2019-03-30 16:22:10 -0400

    Add files via upload

---
+++ deleteQuestion.js
@@ -0,0 +1,21 @@
+app.delete("/questions", function(req, res) {
+  Question.delete("questions").findOneAndDelete({question: req.body.question},
+  function(err, result){
+    if(err) {
+      res.send(500, err);
+    }
+    res.send("Question deleted.")
+  })
+});
+
+fetch({ /* request */}) {
+  .then(res {
+    if(res.ok) {
+      return res.json;
+    }
+  })
+  .then(data {
+    console.log(data);
+    window.location.reload(true);
+  })
+}
